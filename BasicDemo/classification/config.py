59579208c4b87d2d190da95b304ae09ff776c8c6
Lsilense <13518120642@163.com>
2023-09-06 21:36:15 +0800

    modify

--- BasicDemo/classification/config.py
+++ BasicDemo/classification/config.py
@@ -7,7 +7,10 @@ parser.add_argument("-max_len", type=int, default=512)
 parser.add_argument("-batch_size", type=int, default=64)
 parser.add_argument("-num_epochs", type=int, default=10)
 parser.add_argument("-cuda", type=str, default="cuda:4")
-parser.add_argument("-csv_path", type=str, default="/data/zhangxin/code/LLM/glm/tmp_nlptask/BasicDemo/classification/data/ChnSentiCorp_htl_all.csv")
+parser.add_argument(
+    "-csv_path",
+    type=str,
+    default="/data/zhangxin/code/LLM/glm/tmp_nlptask/BasicDemo/classification/data/ChnSentiCorp_htl_all.csv")
 
 # cnn参数配置
 parser.add_argument("-cnn_embedding_dim", type=int, default=200)
